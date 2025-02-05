f3acaf2174bbcb3734e6c70e0511b23eaeec08b7
BunnoBunnoMan <162572632+BunnoBunnoMan@users.noreply.github.com>
2025-02-06 00:23:09 +0100

    Improved Movement
    To Do: Improve Movement

--- PolliFrog/Assets/Scripts/Player Movement.cs
+++ PolliFrog/Assets/Scripts/Player Movement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
     public BoxCollider2D collider;
     public int topSpeed;
     public int moveSpeed;
+    public int jumpStrength;
+    private float jumpTime = 0.0f;
     private Vector2 movement;
     private bool yump = false;
     private bool yumping = false;
@@ -26,10 +28,15 @@ public class PlayerMovement : MonoBehaviour
         movement = Vector2.ClampMagnitude(movement, 1);
         rb.AddForce(movement * moveSpeed);
         //Debug.Log(movement * moveSpeed);
-        Debug.Log(rb.linearVelocityX);
-        if (yump && yumping == false) rb.AddForceY(5, ForceMode2D.Impulse);
+        //Debug.Log(rb.linearVelocityX);
+        if (yump && yumping == false) rb.AddForceY(jumpStrength, ForceMode2D.Impulse);
         if (rb.linearVelocityX > topSpeed) rb.linearVelocityX = topSpeed;
         else if (rb.linearVelocityX < -topSpeed) rb.linearVelocityX = -topSpeed;
+        if (yumping) jumpTime += Time.fixedDeltaTime;
+        else jumpTime = 0;
+        Debug.Log(jumpTime);
+        if (jumpTime > 2 || (yumping && yump == false)) rb.gravityScale = 10;
+        else rb.gravityScale = 1;
     }
 
     public void OnCollisionEnter2D(Collision2D coll)
